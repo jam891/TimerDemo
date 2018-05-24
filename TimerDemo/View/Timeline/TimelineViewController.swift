c33c4514ed23748346bf10a974583cfcd3e44596
jam891 <jam891@gmail.com>
2018-05-24 22:37:15 +0300

    added transport & timeline controllers.

---
+++ TimerDemo/View/Timeline/TimelineViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TimelineViewController.swift
+//  TimerDemo
+//
+//  Created by Vitaliy Delidov on 5/24/18.
+//  Copyright © 2018 Vitaliy Delidov. All rights reserved.
+//
+
+import Cocoa
+
+class TimelineViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ TimerDemo/View/Transport/TransportViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TransportViewController.swift
+//  TimerDemo
+//
+//  Created by Vitaliy Delidov on 5/24/18.
+//  Copyright © 2018 Vitaliy Delidov. All rights reserved.
+//
+
+import Cocoa
+
+class TransportViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
