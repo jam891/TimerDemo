68ebae736a01f845b2a37bb2fa31c2a333872453
jam891 <jam891@gmail.com>
2018-05-24 22:11:36 +0300

    Initial Commit

---
+++ TimerDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TimerDemo
+//
+//  Created by Vitaliy Delidov on 5/24/18.
+//  Copyright © 2018 Vitaliy Delidov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TimerDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TimerDemo
+//
+//  Created by Vitaliy Delidov on 5/24/18.
+//  Copyright © 2018 Vitaliy Delidov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
